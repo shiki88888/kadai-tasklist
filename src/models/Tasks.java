5c638e46f411f563278c3e01600e315a092d6923
kakit <kakit@DESKTOP-OF5JBDK>
2024-06-01 20:55:57 +0900

    モデルクラス名修正
    persistence.xmlファイルのUnitName修正

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 
     @Entity
     @Table(name = "tasks")
-    public class tasks {
+    public class Tasks {
         @Id
         @Column(name = "id")
         @GeneratedValue(strategy = GenerationType.IDENTITY)
